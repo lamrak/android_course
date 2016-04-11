2b7163f64b4a42cf0acbcc53a58282da2131ccd3
odobrunov <oleksii.dobrunov@mads.com>
2016-04-11 12:28:31 +0400

    Update: added api key via gradle.prop and got json result as string

--- app/src/main/java/android/validcat/net/androidcourse/MainActivityFragment.java
+++ app/src/main/java/android/validcat/net/androidcourse/MainActivityFragment.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.validcat.net.androidcourse.network.MovieFetcherAsync;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
     private RecyclerView rv;
 
     @Override
@@ -23,6 +27,22 @@ public class MainActivityFragment extends Fragment {
         MovieAdapter mAdapter = new MovieAdapter();
         rv.setAdapter(mAdapter);
         rv.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
+        MovieFetcherAsync task = new MovieFetcherAsync(new MovieFetcherAsync.IResultListener() {
+            @Override
+            public void onResult(String result) {
+                // result ok
+                Log.d(LOG_TAG, "Server Result=" + result);
+            }
+
+            @Override
+            public void onError(String error) {
+                // error
+                Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //BuildConfig.OPEN_WEATHER_MAP_API_KEY
+        task.execute("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + BuildConfig.OPEN_MOVIE_DB_API_KEY);
 
         return root;
     }
